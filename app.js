fa7772f63f902e5a378054cc0c4c3140b55ac2d2
teeteetee <magazinnii@yandex.ru>
2014-04-23 16:38:10 +0400

    First

---
+++ app.js
@@ -0,0 +1,88 @@
+var express = require('express');
+var http = require('http');
+var path = require('path');
+var favicon = require('static-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var routes = require('./routes');
+//var users = require('./routes/user');
+//var searchresults = require('./routes/searchresults');
+
+var mongo = require('mongodb');
+var monk = require('monk');
+var db = monk('localhost:27017/tav');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(favicon('public/images/favicon.ico'));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname + '/public')));
+app.use(app.router);
+
+//app.get('/', routes.index);
+//app.get('/users', users.list);
+
+app.get('/', function (req,res) {
+    res.sendfile('public/bootstrap/index.html')
+});
+app.get('/places/:placename', function (req,res) {
+    console.log(req.params.placename);
+    res.sendfile('public/bootstrap/ppXTRBIG.html')
+});
+
+
+app.post('/search', function(req,res) {
+    var b = req.body.search;
+    console.log(b);
+   places = db.get('places');
+   var docs;
+    places.findOne({name:b},function(err,docs){
+        console.log(docs);
+      res.render('searchresults',{mainpreview : docs.mainpreview , textpreview : docs.textpreview, redir : docs.redir});
+    });
+    
+    
+
+  
+});
+
+/// catch 404 and forwarding to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+/// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+
+module.exports = app;
