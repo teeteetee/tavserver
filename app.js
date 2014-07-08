2c6c84ecb805856852ee37f92b05a0e18b2622a8
teeteetee <magazinnii@yandex.ru>
2014-07-08 13:29:07 +0400

    CHAT

--- app.js
+++ app.js
@@ -32,7 +32,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 //AS TEMPORARY
 app.get('/chat',function(){
-  var userid = Math.random().toString().slice(2,8);
+
+  var userid = Math.random().toString().slice(2,9);
   console.log('request form chat,created id: '+userid);
   function checkid(vuserid) {
     
@@ -40,7 +41,7 @@ app.get('/chat',function(){
       if(doc!=undefined)
       {
         console.log(doc);
-        res.render('chat',{'adress':userid});
+        res.render('chat',{'adress': vuserid});
         chat.insert({id : vuserid});
         return true
       }
@@ -50,7 +51,7 @@ app.get('/chat',function(){
   
   for (x=false;x!=true;x=false)
     { 
-      var userid = Math.random().toString().slice(2,8);
+      var userid = Math.random().toString().slice(2,9);
       x = checkid(userid);
       console.log('loop cycle: '+x);
     }
