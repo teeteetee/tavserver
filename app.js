e0b6d75f03cfcae3f5027cd2f70eb92eaf6a7c4c
teeteetee <magazinnii@yandex.ru>
2014-11-04 17:34:17 +0400

    --

--- app.js
+++ app.js
@@ -450,7 +450,7 @@ app.post('/uploadauth', function(req,res){
 app.post('/enquery/:hostel/:price', function(req,res){
   x = req.params.hostel;
   y = req.params.price;
-  z = req.body;
+  z = req.body.coco;
   console.log(z);
   res.send(' HOSTEL IS: '+x+'\n'+'PRICE IS: '+y+'\n'+'BODY IS: '+z)
 });
