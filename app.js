b007db54a327781f3716b9591d36e5ec29d10e12
teeteetee <magazinnii@yandex.ru>
2014-12-12 00:50:31 +0400

    --

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.use(sessions({
 var lguser = {};
 app.use(function(req,res,next){
   console.log("CHECKING COOKIES: "+JSON.stringify(req.session)+" "+req.session.lgn);
-   if(req.session.admin){next();}
+   if(req.session.admin === 1){next();}
    if(req.session && req.session.lgn){
      users.findOne({mail:req.session.mail},function(err,user){
       console.log('found user: '+JSON.stringify(user));
