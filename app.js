1c872879036a35486fdcafb61b5fadf140c887f5
teeteetee <magazinnii@yandex.ru>
2014-12-12 00:36:07 +0400

    --

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ app.use(function(req,res,next){
         next();
       }
       else {
-        if(user.length>0){
+        if(user){
         lguser = user;
         delete lguser.phr;
         req.session = lguser;
