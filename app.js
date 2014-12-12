b4f74ffe1c9223f1cec61806471a829ec6e9c538
teeteetee <magazinnii@yandex.ru>
2014-12-12 16:15:08 +0400

    --

--- app.js
+++ app.js
@@ -666,6 +666,7 @@ app.get('/hostels/:hostel',function(req,res){
   }
   else {
     hostels.find({placename:word},function(err,hostel){
+      console.log('hostel.pano is: '+hostel.pano+', has offers: '+hostel.offrqntt);
       if (hostel.length > 0) {
         if (hostel.pano === 1)
        {
